7cdcf91d18f1a1a5aa9859c8d003dc8dbf85af3b
天朝红雨 <178668028@qq.com>
2019-03-15 16:23:42 +0800

    1.PeriodUuidEntity 的创建时间、更新时间增加全文索引注解。 2.visitor支持全文索引,并且手机号码字段名改为mobile

--- xo-starter-demo-core/src/main/java/com/github/jnoee/xo/starter/demo/core/entity/base/PeriodUuidEntity.java
+++ xo-starter-demo-core/src/main/java/com/github/jnoee/xo/starter/demo/core/entity/base/PeriodUuidEntity.java
@@ -3,6 +3,9 @@ package com.github.jnoee.xo.starter.demo.core.entity.base;
 import com.github.jnoee.xo.jpa.entity.UuidEntity;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.SortableField;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -22,9 +25,13 @@ public class PeriodUuidEntity extends UuidEntity {
 
   /** 实体创建时间 */
   @CreatedDate
+  @Field(analyze = Analyze.NO)
+  @SortableField
   private LocalDateTime createTime;
 
   /** 实体更新时间 */
   @LastModifiedDate
+  @Field(analyze = Analyze.NO)
+  @SortableField
   private LocalDateTime updateTime;
 }
--- xo-starter-demo-core/src/main/java/com/github/jnoee/xo/starter/demo/core/entity/visit/Visitor.java
+++ xo-starter-demo-core/src/main/java/com/github/jnoee/xo/starter/demo/core/entity/visit/Visitor.java
@@ -9,6 +9,10 @@ import lombok.Setter;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.Type;
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.IndexedEmbedded;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -16,6 +20,7 @@ import javax.persistence.*;
 /**
  * 访客。
  */
+@Indexed
 @Entity
 @Table(name = "visit_visitor")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
@@ -24,30 +29,37 @@ import javax.persistence.*;
 public class Visitor extends PeriodUuidEntity implements AuthUser {
 
   /** 昵称 */
+  @Field(analyze = Analyze.NO)
   @LogField(text = "昵称")
   @Length(message = "昵称长度应该为1~20字符", min = 1, max = 20)
   private String nickName;
 
+  /** 头像 */
+  private String imgUrl;
+
   /** 手机号码 */
+  @Field(analyze = Analyze.NO)
   @LogField(text = "手机号码")
-  private String phone;
+  private String mobile;
 
   /** 密码 */
   private String password;
 
   /** 身份 */
+  @IndexedEmbedded(includePaths = {"id"})
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "identityId")
   private Identity identity;
 
   /** 启用状态 */
+  @Field(analyze = Analyze.NO)
   @LogField(text = "启用状态")
   @Type(type = "IEnum")
   private EnabledStatus status = EnabledStatus.ENABLED;
 
   @Override
   public String getUsername() {
-    return phone;
+    return mobile;
   }
 
   @Override
